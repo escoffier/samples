49b961c26a8c8c0032f70d5c130e8f50c1f62bcb
unknown <qunfengqiu@gmail.com>
2014-08-15 17:08:04 +0800

    update

--- sqlite3/main.cpp
+++ sqlite3/main.cpp
@@ -2,7 +2,7 @@
 #include <pthread.h>
 #include <iostream>
 
-#define NUM_THREADS 20
+#define NUM_THREADS 10
 
 sqlite3* db = 0;
 void* worker(void* arg)
@@ -12,7 +12,7 @@ void* worker(void* arg)
     //sqlite3_open("npsipgateway.db", &db);
     const char* tail = 0;
     sqlite3_stmt* pVM = 0;
-    sqlite3_prepare_v2(db, "select *from catalog", -1, &pVM, &tail);
+    sqlite3_prepare_v2(db, "select *from information", -1, &pVM, &tail);
     
     int nRet = sqlite3_step(pVM);
     
@@ -26,19 +26,81 @@ void* worker(void* arg)
     else if (nRet == SQLITE_ROW)
     {
         // at least 1 row
-    std::cout<<"thread: "<<(int)arg<<"have rows"<<std::endl;    
-        return 0;
+        std::cout<<"thread: "<<(int)arg<<" have rows"<<std::endl;    
+        //return 0;
     }
+	std::cout<<mnCols<<std::endl;
     sqlite3_finalize(pVM);
     pthread_exit(NULL);
 }
 
+void insert(const char* name, const char *id)
+{
+	sqlite3_stmt* pVM = 0;
+	const char *tail;
+	int ret = 0;
+    const char *error = 0;
+
+    ret = sqlite3_prepare_v2(db, "insert into information values(?, ?);", -1, &pVM, &tail);
+	if(SQLITE_OK == ret)
+	{
+		sqlite3_bind_text(pVM, 1, name, -1 , SQLITE_TRANSIENT);
+		sqlite3_bind_text(pVM, 2, id, -1, SQLITE_TRANSIENT);
+		ret = sqlite3_step(pVM);
+		if(SQLITE_DONE == ret)
+		{
+			//sqlite3_changes(pVM);
+			sqlite3_reset(pVM);
+		}
+		else
+		{
+			ret = sqlite3_reset(pVM);
+            error = sqlite3_errmsg(db);
+			std::cout<<"step error: "<<error<<std::endl;
+		}
+	}
+}
+
+void * insertwork(void *arg)
+{
+	char name[20] = {0};
+	char id[20] = {0};
+
+	sprintf(name, "name-%04d", (int)arg);
+	sprintf(id, "1000%04d", (int)arg);
+	insert(name, id);
+}
+
+void bind(sqlite3_stmt* pVM, int feild, const char* value)
+{
+	int ret = 0;
+
+	ret = sqlite3_bind_text(pVM, feild, value, -1, SQLITE_TRANSIENT);
+	if(SQLITE_OK != ret)
+	{
+		std::cout<<"bind fail: "<<ret<<std::endl;
+		return;
+	}
+}
 
 int main(int argc, char **argv )
 {
 
     sqlite3_open("npsipgateway.db", &db);
-    pthread_t threads[NUM_THREADS];
+	//char *error;
+	//int ret = 0;
+	//ret = sqlite3_exec(db, "create table information(name char(20), id char(20));", 0, 0, &error);
+	//if(0 == ret)
+	//{
+	//	sqlite3_changes(db);
+	//}
+	//else
+	//{
+	//	std::cout<<"create tabke failed, errror is: "<<error<<std::endl;
+	//	return 0;
+	//}
+	insert("robbie", "122");
+    pthread_t threads[2* NUM_THREADS];
     pthread_attr_t attr;
     
     pthread_attr_init(&attr);
@@ -48,12 +110,17 @@ int main(int argc, char **argv )
     {
         pthread_create(&threads[i], &attr, worker, (void*)i);
     }
-    
-    pthread_attr_destroy(&attr);
-    for(i = 0; i < NUM_THREADS; ++i)
+
+    for(i = NUM_THREADS; i <(2 * NUM_THREADS); ++i)
+    {
+        pthread_create(&threads[i], &attr, insertwork, (void*)i);
+    }   
+    //pthread_attr_destroy(&attr);
+    for(i = 0; i < 2*NUM_THREADS; ++i)
     {
         pthread_join(threads[i], NULL);
     }
+    pthread_attr_destroy(&attr);
     pthread_exit(NULL);
     sqlite3_close(db);
     return 0;
